7e5008048c8cf1bbe80de17a3438f1beb0392b77
DatBen <benjamindumont@live.com>
2022-05-24 15:25:41 +0200

    update

--- flute.py
+++ flute.py
@@ -4,9 +4,16 @@ image = imageio.read("test2.png")
 data = image.get_data(0)
 
 f = open("include/flute.h","w")
+g = open("flute.cl","w")
 taille = data.shape
 f.write("#define SIZE_X %s\n" % taille[1] )
 f.write("#define SIZE_Y %s\n" % taille[0])
+g.write("#define SIZE_X %s\n" % taille[1] )
+g.write("#define SIZE_Y %s\n" % taille[0])
+g.write("#define LATICE_D 2 \n")
+g.write("#define LATICE_Q 9\n")
+g.write("#define NUMEL %s\n" % (taille[0]*taille[1]))
+
 
 noirs = []
 rouges = []
@@ -24,4 +31,17 @@ f.write(", ".join([str(x) for x in noirs]))
 f.write("};\n")  
 f.write("int nb_blow = %s;\n" % len(rouges))
 f.write("int blow[]={%s};\n" % ",".join([str(x) for x in rouges]))
-f.close()
+g.write("__constant int nb_wall = %s;\n" % len(noirs)) 
+g.write("__constant int wall[] = {") 
+g.write(", ".join([str(x) for x in noirs]))
+g.write("};\n")  
+g.write("__constant int nb_blow = %s;\n" % len(rouges))
+g.write("__constant int blow[]={%s};\n" % ",".join([str(x) for x in rouges]))
+g.write("__constant int dir[3] = {1,5,8}\n")
+g.write("__constant int dir_fs[3] = {3,6,7}\n")
+
+g.write("\n\n");
+g.close()
+
+with open('flute.cl', 'a') as output, open('flute_template.txt', 'r') as input:
+    output.write(input.read())
